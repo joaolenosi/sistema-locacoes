a7b9160e6656f7406ff3112866b75543d3d82ab1
joaolenosi <joaolenosi@gmail.com>
2026-02-13 14:07:09 -0300

    Add asset helper to BaseController for enhanced asset management capabilities

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -35,7 +35,7 @@ abstract class BaseController extends Controller
      *
      * @var list<string>
      */
-    protected $helpers = ['url', 'empresa'];
+    protected $helpers = ['url', 'empresa', 'asset'];
 
     /**
      * Be sure to declare properties for any property fetch you initialized.
